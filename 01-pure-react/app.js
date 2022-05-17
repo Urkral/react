5b90d1c85c2b5ed12806fa7729066082f790fb5f
Urkral <esteban.dutrannois@outlook.com>
2022-05-17 10:19:22 +0200

    second commit

--- 01-pure-react/app.js
+++ 01-pure-react/app.js
@@ -2,12 +2,22 @@ const App = () =>{
     return React.createElement(
         "div",
         {},
-        React.createElement(
-            "h1",
-            // En js on utilise className car la mot class est réservé 
-            {className: "bg-red-500"},
-            "Hello World !",
-        ),
+        // on peut créer plusieurs élément a l'aide d'un array
+    [   
+       React.createElement(
+       "h1",
+       // En js on utilise className car la mot class est réservé 
+       {className: "bg-red-500"},
+       "Hello World !",
+   ),
+       React.createElement(
+       "h2",
+       // En js on utilise className car la mot class est réservé 
+       {className: "bg-red-500"},
+       "GOULAGWE!",
+   )
+
+    ]
     )
 }
 // Nous devons faire une ReactDom.render pour afficher le react 
